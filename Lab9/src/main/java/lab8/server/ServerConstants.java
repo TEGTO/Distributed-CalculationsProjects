27887277e7d57b12b61caed22b2c246150d1d9f3
TEGTO <90476119+TEGTO@users.noreply.github.com>
2023-11-30 20:46:07 +0200

    commit18
    updated mysql for docker container connection

--- Lab9/src/main/java/lab8/server/ServerConstants.java
+++ Lab9/src/main/java/lab8/server/ServerConstants.java
@@ -3,9 +3,9 @@ public interface ServerConstants
 {
     public static final String SERVER_IP = "localhost";
     public static final int SERVER_PORT = 12345;
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/movie_store";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "2004gfgf";
+    public static final String DB_URL = "jdbc:mysql://192.168.0.105:3306/movie_store";
+    public static final String DB_USER = "tegto";
+    public static final String DB_PASSWORD = "";
     public static final String UNIQUE_BINDING_NAME = "server.calculator";
     public static final String QUEUE_NAME = "myServer";
 
